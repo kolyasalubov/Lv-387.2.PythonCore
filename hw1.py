c7ea275580e020fbab66543f7502c52c0caa2873
Vitaliia Oliinyk <vitaliiya.ol@gmail.com>
2019-02-26 11:25:27 +0200

    project from HW1

---
+++ hw1.py
@@ -0,0 +1,27 @@
+#1
+name = input("what's your name?")
+age = input("how old are you?")
+location = input("where do you live?")
+print("your name is " + name)
+print("your age is", int(age))
+print("you live in " + location)
+
+
+#2
+a = int(input("enter first integer"))
+b = int(input("enter second integer"))
+print("a =", a)
+print("b =", b)
+print("a+b=", a+b)
+print("a-b=", a-b)
+
+#3
+var_int = 10
+big_int = var_int*3.5
+print(big_int)
+var_float = 8.4
+var_float-=1
+print(var_float)
+var_str = "No"
+var_str = var_str*2 + "Yes"*3
+print(var_str)
